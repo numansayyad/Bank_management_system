f157a3d45805bd2ffa6bfebd9fa6bba4fa0f631e
Numansayyadd <numansayyad2005@gmail.com>
2026-01-02 18:54:29 +0530

    Initial login screen added

---
+++ Login.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+import java.util.Set;
+
+import javax.swing.*;
+public class Login extends JFrame{
+    Login(){
+
+        setTitle("ATM MACINE");
+        setSize(800,400);
+        setVisible(true);
+        setLocation(350,200);
+    }
+public static void main(String args[]){
+    new Login();
+}
+}
